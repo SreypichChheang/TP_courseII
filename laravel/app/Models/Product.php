6f8d289044928378b8e73e163687be328ca90155
SreypichChheang <chheangsreypich353@gmail.com>
2025-03-16 15:04:39 +0700

    After Migrate

---
+++ laravel/app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    // Define which attributes can be mass-assigned
+    protected $fillable = ['name', 'price', 'category_id'];
+
+}
